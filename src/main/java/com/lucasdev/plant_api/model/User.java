b2694c9573cc7409e603589c615419e1d7f360e3
NlengDominique <luqnleng5@gmail.com>
2024-12-11 16:26:35 +0100

    creation du modele User

---
+++ src/main/java/com/lucasdev/plant_api/model/User.java
@@ -0,0 +1,30 @@
+package com.lucasdev.plant_api.model;
+
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+}
---
+++ src/main/java/com/lucasdev/plant_api/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.lucasdev.plant_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.lucasdev.plant_api.model.*;
+public interface UserRepository extends JpaRepository<User,Long> {
+
+
+
+
+}
