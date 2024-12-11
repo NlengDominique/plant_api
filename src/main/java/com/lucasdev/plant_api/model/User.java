9d5980a7cc9a7b04cb07eecfaf8c34fcaea8e7ec
NlengDominique <luqnleng5@gmail.com>
2024-12-11 16:30:25 +0100

    ajout du README

--- src/main/java/com/lucasdev/plant_api/model/User.java
+++ src/main/java/com/lucasdev/plant_api/model/User.java
@@ -1,7 +1,4 @@
 package com.lucasdev.plant_api.model;
-
-import org.springframework.stereotype.Component;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
